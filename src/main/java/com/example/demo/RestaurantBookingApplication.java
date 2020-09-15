be70b063947b46395717d07a26b62c751f6c7619
Milky <tpdhks0369@gmail.com>
2020-09-15 20:56:34 +0900

    feat : 프로젝트 최초 커밋

---
+++ src/main/java/com/example/demo/RestaurantBookingApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantBookingApplication.class, args);
+    }
+
+}
+
